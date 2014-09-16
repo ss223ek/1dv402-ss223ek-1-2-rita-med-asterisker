939a1488bda875ca4b44e0da2d0ba4ae8bdfd942
ss223ek <ss223ek@student.lnu.se>
2014-09-16 14:05:09 +0200

    Program-ide, bara kommentarer Signed-off-by: ss223ek <ss223ek@student.lnu.se>

---
+++ ss223ek-1-2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ss223ek_1_2
+{
+    class Program
+    {
+        //Rita med asterisker
+        static void Main(string[] args)
+        {
+            // rad 1-25
+            // varannan rad ska skjutas in "ett tomrum"  hitta detta med mudulo2 i if-villkor (skriv ev ut " ")
+            // var tredje rad ska vara gul, hitta detta med modul3 i en switch-sats
+
+            // för varje rad ska det finns kolumn 1-39
+            // (skriv ut " *")
+
+            // (skriv ut radbrytning) innan återhopp i yttre loop
+
+
+
+        }
+    }
+}
