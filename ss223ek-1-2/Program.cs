ce764f65f26d518959b2109994ca0a80b3150a95
ss223ek <ss223ek@student.lnu.se>
2014-09-16 14:26:43 +0200

    Fungerande version för inlämning
    Signed-off-by: ss223ek <ss223ek@student.lnu.se>

--- ss223ek-1-2/Program.cs
+++ ss223ek-1-2/Program.cs
@@ -15,10 +15,47 @@ namespace ss223ek_1_2
             // varannan rad ska skjutas in "ett tomrum"  hitta detta med mudulo2 i if-villkor (skriv ev ut " ")
             // var tredje rad ska vara gul, hitta detta med modul3 i en switch-sats
 
-            // för varje rad ska det finns kolumn 1-39
-            // (skriv ut " *")
+            int rowNumber, colNumber;
 
-            // (skriv ut radbrytning) innan återhopp i yttre loop
+            for (rowNumber = 1; rowNumber < 26; rowNumber++)
+            {
+                if (0 == rowNumber % 2)             //om det är jämn rad, skriv inlednade mellanslag
+                {
+                    Console.Write(" ");
+                }
+
+                switch(rowNumber%3)                 // var tredje
+                {
+                    case 1:
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        break;
+                    case 2:
+                        Console.ForegroundColor = ConsoleColor.Magenta;
+                        break;
+                    case 0:
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        break;
+                                
+                }
+
+                // för varje rad ska det finns kolumn 1-39
+                // (skriv ut " *")
+
+                for (colNumber = 1; colNumber < 40; colNumber++)
+                {
+                    Console.Write("* ");
+
+                }
+                // (skriv ut radbrytning) innan återhopp i yttre loop
+                Console.WriteLine();
+
+            
+            
+            
+            }
+
+            
+
 
 
 
